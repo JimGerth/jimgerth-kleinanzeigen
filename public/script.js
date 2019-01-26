f84c25e0288c0f47d68ead5001f392b22534a1ff
JimGerth <jim.frederic.gerth@gmail.com>
2019-01-26 23:16:40 +0100

    added in master styling, merging it with the new layout. looking quite nice and responsive, some clipping issues remain especially with the button forcing the price to wrap sometimes...

--- public/script.js
+++ public/script.js
@@ -1,5 +1,15 @@
+var articles;
+
+function preload() {
+  articles = loadJSON('content.json');
+}
+
 function setup() {
   noCanvas();
+
+  let templateSource = $('#article-template').html();
+  let template = Handlebars.compile(templateSource);
+  $('#article-list').html(template(articles));
 }
 
 function draw() {
